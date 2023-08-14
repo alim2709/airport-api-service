919256007bf1e9b34552078dc5816b9b432ff4cf
alim2709alim <happy-newyear@mail.ru>
2023-08-14 13:01:41 +0300

    Update models in service/models.py

--- service/models.py
+++ service/models.py
@@ -1,6 +1,8 @@
 import os
 import uuid
 
+from django.utils import timezone
+
 from app import settings
 from country.models import City
 from django.db import models
@@ -14,7 +16,7 @@ class Crew(models.Model):
     last_name = models.CharField(max_length=63)
 
     class Meta:
-        ordering = ("first_name", "last_name")
+        ordering = ("first_name", "last_name",)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -34,7 +36,7 @@ class Airport(models.Model):
         null=True,
         related_name="airports"
     )
-    image = models.ImageField(null=True, upload_to=airport_image_file_path)
+    image = models.ImageField(null=True, upload_to=airport_image_file_path, blank=True)
 
     class Meta:
         ordering = ("name", "closest_big_city")
@@ -48,18 +50,18 @@ class Route(models.Model):
         Airport,
         on_delete=models.SET_NULL,
         null=True,
-        related_name="routes"
+        related_name="sources"
     )
     destination = models.ForeignKey(
         Airport,
         on_delete=models.SET_NULL,
         null=True,
-        related_name="routes"
+        related_name="destinations"
     )
     distance = models.IntegerField()
 
     class Meta:
-        ordering = "source"
+        ordering = ("source",)
 
     def __str__(self):
         return f"{self.source} - {self.destination}"
@@ -69,7 +71,7 @@ class AirplaneType(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
     class Meta:
-        ordering = "name"
+        ordering = ("name",)
 
     def __str__(self):
         return self.name
@@ -79,7 +81,8 @@ class AirCompany(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
     class Meta:
-        ordering = "name"
+        ordering = ("name",)
+        verbose_name_plural = "AirCompanies"
 
     def __str__(self):
         return self.name
@@ -114,6 +117,7 @@ class Flight(models.Model):
     airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, related_name="flights")
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
+    crew = models.ManyToManyField(Crew, related_name="flights")
 
     class Meta:
         ordering = ["route", "-departure_time"]
@@ -121,6 +125,34 @@ class Flight(models.Model):
             models.Index(fields=["route"]),
             models.Index(fields=["departure_time"]),
         ]
+
+    @staticmethod
+    def validate_departure_arrival_time(departure_time, arrival_time, error_to_raise):
+        now = timezone.now()
+        if departure_time < now:
+            raise error_to_raise(
+                {"departure_time": f"Departure time shouldn't be earlier than {now}"}
+            )
+
+        if arrival_time < departure_time:
+            raise error_to_raise(
+                {"arrival_time": f"Arrival time can't be earlier than departure time"}
+            )
+
+    def clean(self):
+        Flight.validate_departure_arrival_time(
+            self.departure_time,
+            self.arrival_time,
+            ValidationError
+        )
+
+    def save(
+            self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.full_clean()
+        return super(Flight, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     def __str__(self):
         return f"{str(self.route)} (departure: {self.departure_time}, arrival: {self.arrival_time})"
