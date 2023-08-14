df8a86a17542f115ac2964a662cb4fc02c8082a8
alim2709alim <happy-newyear@mail.ru>
2023-08-14 13:02:38 +0300

    Add root to service application in app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/service/", include("service.urls", namespace="service")),
     path("__debug__/", include("debug_toolbar.urls")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
