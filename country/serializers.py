1495e1a07ec1009ecaef810ae5ecaeffb6076433
alim2709 <alimselemetow@gmail.com>
2023-08-16 21:00:02 +0300

    Implement serializers & views to country app

--- country/serializers.py
+++ country/serializers.py
@@ -6,16 +6,31 @@ from country.models import City, Country
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ("id", "name", "country")
+        fields = ("id", "name", "country", "airports")
+
+
+class CityListSerializer(CitySerializer):
+    country = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    airports = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
 
 
 class CityDetailSerializer(CitySerializer):
     country = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
 
+    class Meta:
+        model = City
+        fields = ("id", "name", "country")
+
 
 class CountrySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Country
         fields = ("id", "name")
 
+
+class CountryDetailSerializer(CountrySerializer):
+    cities = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
+
+    class Meta:
+        model = Country
+        fields = ("id", "name", "cities")
--- country/views.py
+++ country/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from country.models import Country, City
+from country.serializers import CountrySerializer, CitySerializer, CityListSerializer, CountryDetailSerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = (IsAdminUser,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related("cities")
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return CountryDetailSerializer
+        return CountrySerializer
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all().select_related("country").prefetch_related("airports")
+    serializer_class = CitySerializer
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return CityListSerializer
+        return CitySerializer
