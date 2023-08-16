52b78c202c7a3d0b3ee84fb1344eab7873610126
alim2709 <alimselemetow@gmail.com>
2023-08-16 13:54:28 +0300

    Update service/serializers.py

--- service/serializers.py
+++ service/serializers.py
@@ -1,6 +1,7 @@
 from django.db import transaction
 from rest_framework import serializers
 
+from country.serializers import CityDetailSerializer
 from service.models import (
     Crew,
     Airport,
@@ -30,6 +31,10 @@ class AirportListSerializer(AirportSerializer):
     closest_big_city = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
 
 
+class AirportDetailSerializer(AirportSerializer):
+    closest_big_city = CityDetailSerializer(many=False, read_only=True)
+
+
 class AirportImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airport
@@ -40,6 +45,16 @@ class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
         fields = ("id", "source", "destination", "distance")
+
+
+class RouteListSerializer(RouteSerializer):
+    source = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    destination = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+
+
+class RouteDetailSerializer(RouteSerializer):
+    source = AirportDetailSerializer(many=False, read_only=True)
+    destination = AirportDetailSerializer(many=False, read_only=True)
 
 
 class AirplaneTypeSerializer(serializers.ModelSerializer):
@@ -57,10 +72,29 @@ class AirCompanySerializer(serializers.ModelSerializer):
 class AirplaneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airplane
-        fields = ("id", "name", "rows", "seats_in_row", "airplane_type", "air_company")
+        fields = ("id", "name", "rows", "seats_in_row", "capacity", "airplane_type", "air_company")
+
+
+class AirplaneListSerializer(AirplaneSerializer):
+    airplane_type = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    air_company = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+
+
+class AirplaneDetailSerializer(AirplaneSerializer):
+    airplane_type = AirplaneTypeSerializer(many=False, read_only=True)
+    air_company = AirCompanySerializer(many=False, read_only=True)
 
 
 class FlightSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(FlightSerializer, self).validate(attrs)
+        Flight.validate_departure_arrival_time(
+            attrs["departure_time"],
+            attrs["arrival_time"],
+            serializers.ValidationError
+        )
+        return data
+
     class Meta:
         model = Flight
         fields = ("id", "route", "airplane", "departure_time", "arrival_time", "crew")
@@ -71,10 +105,37 @@ class FlightListSerializer(FlightSerializer):
     crew = serializers.StringRelatedField(many=True, read_only=True)
     airplane = serializers.CharField(source="airplane.name")
     airplane_num_seats = serializers.IntegerField(source="airplane.capacity")
+    tickets_available = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Flight
-        fields = ("id", "route", "airplane", "airplane_num_seats", "departure_time", "arrival_time", "crew")
+        fields = (
+            "id", "route", "airplane", "airplane_num_seats",
+            "tickets_available", "departure_time", "arrival_time", "crew"
+        )
+
+
+class FlightDetailSerializer(FlightSerializer):
+    route = RouteDetailSerializer(many=False, read_only=True)
+    crew = CrewSerializer(many=True, read_only=True)
+    airplane = AirplaneDetailSerializer(many=False, read_only=True)
+    taken_seats = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Flight
+        fields = ("id", "route", "airplane", "taken_seats", "departure_time", "arrival_time", "crew")
+
+    def get_seat_and_row(self, ticket):
+        return {"row": ticket.row, "seat": ticket.seat}
+
+    def get_taken_seats(self, flight):
+        return [self.get_seat_and_row(ticket) for ticket in
+                flight.tickets.all()]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["taken_seats"] = self.get_taken_seats(instance)
+        return data
 
 
 class TicketSerializer(serializers.ModelSerializer):
