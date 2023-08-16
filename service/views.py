49bfae05eea04379f4800fb73fc00377ceba26f7
alim2709 <alimselemetow@gmail.com>
2023-08-16 18:39:10 +0300

    Add DEFAULT_PAGINATION_CLASS to app/settings.py

--- service/views.py
+++ service/views.py
@@ -158,16 +158,9 @@ class FlightViewSet(viewsets.ModelViewSet):
         return FlightSerializer
 
 
-class OrderPagination(PageNumberPagination):
-    page_size = 5
-    page_size_query_param = "page_size"
-    max_page_size = 100
-
-
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    pagination_class = OrderPagination
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
