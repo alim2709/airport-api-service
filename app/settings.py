e856610b75f8477898966d6d0907ac39457675a9
alim2709 <alimselemetow@gmail.com>
2023-08-17 00:51:32 +0300

    Add DEFAULT_AUTHENTICATION_CLASSES to app/settings.py

--- app/settings.py
+++ app/settings.py
@@ -148,6 +148,10 @@ REST_FRAMEWORK = {
 
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 6,
+
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
 
 
