276eb74417d7d2c9fd693b456db14ffa41bff0ed
alim2709 <alimselemetow@gmail.com>
2023-08-17 11:56:52 +0300

    Add DEFAULT_PERMISSION_CLASSES to app/settings.py

--- app/settings.py
+++ app/settings.py
@@ -151,7 +151,10 @@ REST_FRAMEWORK = {
 
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ]
 }
 
 
